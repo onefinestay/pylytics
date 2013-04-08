8c2939147da41c491cf82fabdbcdffb5dfb4de40
Jackson Hull <jackson.hull@plumdistrict.com>
2013-04-08 16:51:33 +0200

    cleanup

--- examples/sales.sql
+++ examples/sales.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `sales` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `location_name` int(11) NOT NULL,
+  `location_name` varchar(255) NOT NULL,
   `sale_value` decimal(12,2) NOT NULL,
   `sale_date` datetime NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
