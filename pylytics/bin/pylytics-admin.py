cf82ac1452d1abfdfd458e733b1bd5dd4caa27d9
Daniel Townsend <townsend51@gmail.com>
2013-04-17 11:08:13 +0100

    started fleshing out project template command some more

--- pylytics/bin/pylytics-admin.py
+++ pylytics/bin/pylytics-admin.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 import sys
 
-if __name__ == "__main__":
-    sys.stdout.write('Under construction.\n')
+import pylytics
+
+
+def create_project(project_name):
+    """
+    Create a new pylytics project.
+    
+    > Get the location of pylytics ... 
+    
+    """
+    # walk ... read ... then write ....
+    pass
+
+
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser(
+        description = "Create new pylytics projects.")
+    parser.add_argument(
+        'project',
+        help = 'The name of the new project',
+        nargs = 1,
+        type = str,
+        )
+
+    args = parser.parse_args().__dict__
+    project = args['project'][0]
+    sys.stdout.write('Creating new project - {} ...\n'.format(project))
+    create_project(project)
