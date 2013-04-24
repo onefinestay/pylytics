58ccc32037e33b2d6f7e26ce286da0530a9a0865
Daniel Townsend <townsend51@gmail.com>
2013-04-24 18:12:23 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
                   'pylytics.conf.project_template.fact': ['sql/*']},
     scripts = ['pylytics/bin/pylytics-admin.py'],
     url='https://github.com/onefinestay/pylytics',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     )
