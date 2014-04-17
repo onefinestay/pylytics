4520039eb3ca5b21b071a42c699763377163d32f
Daniel Townsend <townsend51@gmail.com>
2014-04-17 17:17:26 +0100

    added most recent dim_timezone

--- pylytics/conf/project_template/dim/dim_example.py
+++ pylytics/conf/project_template/dim/dim_example.py
@@ -3,7 +3,9 @@ from pylytics.library.dim import Dim
 
 class DimExample(Dim):
     """Example Dimension."""
+
     source_db = 'example'  # The database to run this source query on.
+
     source_query = """
         SELECT DISTINCT `foo` FROM `bar`;
         """
