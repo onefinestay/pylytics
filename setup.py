b132f5dc805e46968f7d8214b5e6141a547975b6
Daniel Townsend <townsend51@gmail.com>
2014-11-25 15:46:29 +0000

    adding missing files to manifest

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
         'pylytics/bin/pylytics-admin.py',
     ],
     url='https://github.com/onefinestay/pylytics',
-    version='1.0.6',
+    version='1.0.7',
     zip_safe=False,
     )
