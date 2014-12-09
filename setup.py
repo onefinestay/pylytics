af92dd0c3a23d2117515e4107dcfc5e757ca98bc
Daniel Townsend <townsend51@gmail.com>
2014-12-09 22:33:04 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
         'pylytics/bin/pylytics-admin.py',
     ],
     url='https://github.com/onefinestay/pylytics',
-    version='1.0.8',
+    version='1.0.9',
     zip_safe=False,
     )
