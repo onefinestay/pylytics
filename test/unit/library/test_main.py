92bf288cafbd3c3c27c678d57bca1187aa9a773a
Daniel Townsend <townsend51@gmail.com>
2014-12-19 16:31:18 +0000

    added test for isolation

--- test/unit/library/test_main.py
+++ test/unit/library/test_main.py
@@ -4,7 +4,8 @@ from mock import Mock, patch
 import pytest
 from pytz import UTC
 
-from pylytics.library.main import find_scheduled
+from pylytics.library.main import find_scheduled, Commander
+from pylytics.library.fact import Fact
 from pylytics.library.schedule import Schedule
 
 from test.dummy_project import Sales
@@ -42,3 +43,22 @@ def test_find_scheduled(get_now, schedule, should_run):
     scheduled_facts = find_scheduled([SalesMock])
     present = SalesMock in scheduled_facts
     assert present == should_run
+
+
+@patch('pylytics.library.main.get_all_fact_classes')
+def test_isolation(get_all_fact_classes):
+    """ Make sure that subsequent facts get called, even if the first
+    fact fails.
+    """
+
+    class FirstFact(Fact):
+        def update(self):
+            raise Exception
+
+    class SecondFact(Fact):
+        update = Mock()
+
+    get_all_fact_classes.return_value = [FirstFact, SecondFact]
+    commander = Commander()
+    commander.run('update', 'FirstFact', 'SecondFact')
+    assert SecondTest.update.called
